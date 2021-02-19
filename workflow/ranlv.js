611824667190762bf2f9c11f74ddb4314887053c
q154327 <76770515+q154327@users.noreply.github.com>
2021-02-19 16:05:59 +0000

    Update ranlv.js

--- workflow/ranlv.js
+++ workflow/ranlv.js
@@ -96,6 +96,8 @@ if ($.isNode()) {
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
  } else {
+    rlurlArr.push($.getdata('rlurl'))
+    rlheaderArr.push($.getdata('rlheader'))
     rlbodyArr.push($.getdata('rlbody'))
     let rlcount = ($.getval('rlcount') || '1');
   for (let i = 2; i <= rlcount; i++) {
