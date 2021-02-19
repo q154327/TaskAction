4540f0bcf58dfbdd83044efffbce52265b5a43a5
q154327 <76770515+q154327@users.noreply.github.com>
2021-02-19 13:28:38 +0000

    Update ranlv.js

--- workflow/ranlv.js
+++ workflow/ranlv.js
@@ -87,16 +87,14 @@ if ($.isNode()) {
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
  } else {
-//    rlurlArr.push($.getdata('rlurl'))
-//    rlheaderArr.push($.getdata('rlheader'))
-//    rlbodyArr.push($.getdata('rlbody'))
-//    let rlcount = ($.getval('rlcount') || '1');
-//  for (let i = 2; i <= rlcount; i++) {
-//    rlurlArr.push($.getdata(`rlurl${i}`))
-//    rlheaderArr.push($.getdata(`rlheader${i}`))
-//    rlbodyArr.push($.getdata(`rlbody${i}`))
-	rlurl = process.env.RLURL.split('\n')
-	rlheader = process.env.RLHEADER.split('\n')
+    rlurlArr.push($.getdata('rlurl'))
+    rlheaderArr.push($.getdata('rlheader'))
+    rlbodyArr.push($.getdata('rlbody'))
+    let rlcount = ($.getval('rlcount') || '1');
+  for (let i = 2; i <= rlcount; i++) {
+    rlurlArr.push($.getdata(`rlurl${i}`))
+    rlheaderArr.push($.getdata(`rlheader${i}`))
+    rlbodyArr.push($.getdata(`rlbody${i}`))
   }
 }
 !(async () => {
